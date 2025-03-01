40b2f89fa6695d1843c74f6728b23518e88ee69d
Tyler Smith <tsmith105@unl.edu>
2025-03-01 13:24:54 -0600

    Update encrypt.py

--- app/utils/encryption/encrypt.py
+++ app/utils/encryption/encrypt.py
@@ -8,8 +8,20 @@ def encrypt_data(data, key):
     cipher = AES.new(key, AES.MODE_GCM)
     ciphertext, tag = cipher.encrypt_and_digest(data)
     return base64.b64encode(cipher.nonce + tag + ciphertext).decode()
+
+def decrypt_data(encrypted_data, key):
+    data = base64.b64decode(encrypted_data)
+    nonce, tag, ciphertext = data[:16], data[16:32], data[32:]
+    cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+    return cipher.decrypt_and_verify(ciphertext, tag).decode()
+
     
 message = "Hello, World!"
 encrypted_message = encrypt_data(message.encode(), key)
 
 print(encrypted_message)
+
+decrypted_message = decrypt_data(encrypted_message, key)
+
+print(decrypted_message)
+
