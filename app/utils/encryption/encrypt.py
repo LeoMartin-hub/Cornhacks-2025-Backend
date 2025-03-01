05bf5548f8787e76c5330b58b6f9212426b62287
Tyler Smith <tsmith105@unl.edu>
2025-03-01 12:21:37 -0600

    Update encrypt.py

--- app/utils/encryption/encrypt.py
+++ app/utils/encryption/encrypt.py
@@ -5,4 +5,11 @@ import base64
 key = get_random_bytes(16)
 
 def encrypt_data(data, key):
+    cipher = AES.new(key, AES.MODE_GCM)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+    return base64.b64encode(cipher.nonce + tag + ciphertext).decode()
     
+message = "Hello, World!"
+encrypted_message = encrypt_data(message.encode(), key)
+
+print(encrypted_message)
