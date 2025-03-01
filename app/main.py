c7378d4fe299fb755ed4f91938f4fea90323d5cf
Leo Martin <lmartin32@huskers.unl.edu>
2025-03-01 13:28:02 -0600

    main update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from app.routes.photos import router as photos_router
 
 app = FastAPI()
+
+# Include routes
+app.include_router(photos_router, prefix="/api")
 
 @app.get("/")
 def read_root():
