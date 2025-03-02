530e2787381df91ad04a8042cdd9762703724840
Tyler Smith <tsmith105@unl.edu>
2025-03-01 19:45:08 -0600

    Removed Test code

--- DockerSetup/app/encryption/decrypt.py
+++ DockerSetup/app/encryption/decrypt.py
@@ -10,12 +10,3 @@ def decrypt_data(encrypted_data, key):
     nonce, tag, ciphertext = data[:16], data[16:32], data[32:]
     cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
     return cipher.decrypt_and_verify(ciphertext, tag).decode()
-
-message = "Hello, World!"
-encrypted_message = encrypt.encrypt_data(message.encode(), key)
-
-print(encrypted_message)
-
-decrypted_message = decrypt_data(encrypted_message, key)
-
-print(decrypted_message)
--- DockerSetup/app/models/encrypt_package.py
+++ DockerSetup/app/models/encrypt_package.py
@@ -14,10 +14,3 @@ def encrypt_package(unencrypted_package):
     encrypted_image = encrypt.encrypt_data(image_data, encrypt.key)
     return encrypted_package.EncryptedPackage(encrypted_message, encrypted_image, unencrypted_package.date, unencrypted_package.time)
     
-    
-package = unencrypted_package.UnencryptedPackage("Hello, World!", "app/storage/uploaded/example_uploaded.jpeg", "2021-01-01", "12:00:00")
-key = encrypt.key
-encrypted_package = encrypt_package(package)
-print(encrypted_package.get_encrypted_message())
-print(encrypted_package.get_encrypted_image())
-print(key)
